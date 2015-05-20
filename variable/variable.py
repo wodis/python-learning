30c865352653aaf6798779d1a2f3c8d8b26ab1c3
wudi <di.wu@yolu-inc.com>
2015-05-20 11:29:58 +0800

    encoding & variable

--- variable/variable.py
+++ variable/variable.py
@@ -8,3 +8,16 @@ name = "John" # 字符串
 print counter
 print miles
 print name
+
+a = b = c = 1
+
+print a, b, c
+
+str = "my name is Dan"
+
+print str # 输出完整字符串
+print str[0] # 输出字符串中的第一个字符
+print str[2:5] # 输出字符串中第三个至第五个之间的字符串
+print str[2:] # 输出从第三个字符开始的字符串
+print str * 2 # 输出字符串两次
+print str + "TEST" # 输出连接的字符串
