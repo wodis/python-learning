4c401e2d9e943c7945c365a7e6f208b6b16bfce2
wudi <di.wu@yolu-inc.com>
2015-05-20 14:32:22 +0800

    change file name

---
+++ decision/decision.py
@@ -0,0 +1,23 @@
+# encoding: utf-8
+__author__ = 'wudi'
+
+flag = False
+name = 'luren'
+if name == 'python':         # 判断变量否为'python'
+    flag = True          # 条件成立时设置标志为真
+    print 'welcome boss'    # 并输出欢迎信息
+else:
+    print name              # 条件不成立时输出变量名称
+
+
+num = 5
+if num == 3:            # 判断num的值
+    print 'boss'
+elif num == 2:
+    print 'user'
+elif num == 1:
+    print 'worker'
+elif num < 0:           # 值小于零时输出
+    print 'error'
+else:
+    print 'roadman'     # 条件均不成立时输出
--- variable/variable.py
+++ variable/variable.py
@@ -21,3 +21,38 @@ print str[2:5] # 输出字符串中第三个至第五个之间的字符串
 print str[2:] # 输出从第三个字符开始的字符串
 print str * 2 # 输出字符串两次
 print str + "TEST" # 输出连接的字符串
+
+list = [ 'abcd', 786 , 2.23, 'john', 70.2 ]
+tinylist = [123, 'john']
+
+print list # 输出完整列表
+print list[0] # 输出列表的第一个元素
+print list[1:3] # 输出第二个至第三个的元素
+print list[2:] # 输出从第三个开始至列表末尾的所有元素
+print tinylist * 2 # 输出列表两次
+print list + tinylist # 打印组合的列表
+
+tuple = ( 'abcd', 786 , 2.23, 'john', 70.2 )
+tinytuple = (123, 'john')
+
+print tuple # 输出完整元组
+print tuple[0] # 输出元组的第一个元素
+print tuple[1:3] # 输出第二个至第三个的元素
+print tuple[2:] # 输出从第三个开始至列表末尾的所有元素
+print tinytuple * 2 # 输出元组两次
+print tuple + tinytuple # 打印组合的元组
+
+dict = {}
+dict['one'] = "This is one"
+dict[2] = "This is two"
+
+tinydict = {'name': 'john','code':6734, 'dept': 'sales'}
+
+
+print dict['one'] # 输出键为'one' 的值
+print dict[2] # 输出键为 2 的值
+print tinydict # 输出完整的字典
+print tinydict.keys() # 输出所有键
+print tinydict.values() # 输出所有值
+
+print
